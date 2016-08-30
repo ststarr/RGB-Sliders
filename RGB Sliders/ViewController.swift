5467f5657a63f6aa3e38cf55b93cccccdd73b7fb
Al Hammons <susan.tamilyn@gmail.com>
2016-08-30 12:25:17 -0500

    Lesson 2.2 completion RGB Sliders initial app before navigation

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RedSlider: UISlider!
+    
+    @IBOutlet weak var GreenSlider: UISlider!
+    
+    @IBOutlet weak var BlueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateBackgroundColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateBackgroundColor() {
+        let red = CGFloat(RedSlider.value)
+        let green = CGFloat(GreenSlider.value)
+        let blue = CGFloat(BlueSlider.value)
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
 
 }
 
